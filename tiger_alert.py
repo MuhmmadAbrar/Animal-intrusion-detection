cce92eb50f59204b7c9b6dde8394a6930fc63ba0
Muhmmad Abrar <88892675+MuhmmadAbrar@users.noreply.github.com>
2023-08-05 07:01:25 +0530

    Removed Twilio Creds

--- tiger_alert.py
+++ tiger_alert.py
@@ -6,14 +6,14 @@ now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
 
-account_sid = 'AC5eba9221c451d967e1825bfbee67328c'
-auth_token = 'ff715bd2c5598d66af3dba1ce3faead5'
+account_sid = '[YOUR_TWILLIO_SID]'
+auth_token = '[YOUR AUTH_TOKEN]'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-  from_='+13253265391',
+  from_='[YOUR TWILLIO NUMBER]',
   body='Tiger detected on '+current_time,
-  to='+917305366617'
+  to='[YOUR PHONE NUMBER]'
 )
 
 print(message.sid)
